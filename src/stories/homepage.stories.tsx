21c55e1ab8cc523baa4d7abef6b93de7d0938ade
Valeri Sabev <valchygaming@gmail.com>
2022-03-07 21:22:29 +0100

    GitHub actions bug fix

--- src/stories/homepage.stories.tsx
+++ src/stories/homepage.stories.tsx
@@ -1,15 +1,15 @@
-import React from 'react'
+import React from 'react';
 
-import App from '../pages/index'
+import App from '../pages/index';
 
 export default {
-  // this is where it will appear on the storybook sidebar
-  title: 'My Project/Home Page',
-  component: App,
-}
+	// this is where it will appear on the storybook sidebar
+	title: 'My Project/Home Page',
+	component: App
+};
 
-const Template = (args) => <App {...args} />
+const Template: any = args => <App {...args} />;
 
-export const IndexPage = Template.bind({})
+export const IndexPage = Template.bind({});
 // default props it will render with
-IndexPage.args = {}
+IndexPage.args = {};
